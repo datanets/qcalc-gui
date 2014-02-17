c5997b129d8b8fd8e93283b374b3fd83bf76c57b
summea <chkmyemailnow@yahoo.com>
2014-02-16 20:06:40 -0800

    gui updates

--- qcalc-gui/qcalc-gui/MainWindow.xaml.cs
+++ qcalc-gui/qcalc-gui/MainWindow.xaml.cs
@@ -30,30 +30,67 @@ namespace qcalc_gui
         {
             Quaternion quatA = new Quaternion(0,0,0,0);
             Quaternion quatB = new Quaternion(0,0,0,0);
-            Quaternion result = new Quaternion(0,0,0,0);
-
-            quatA = parseStringToQuat(QuatABox.Text);
-            quatB = parseStringToQuat(QuatBBox.Text);
+            Quaternion quatResult = new Quaternion(0,0,0,0);
+            Double doubleResult = 0.0f;
 
             switch (op)
             {
                 case "add":
-                    result = quatA.add(quatB);
+                    quatA = parseStringToQuat(OPQuatABox.Text);
+                    quatB = parseStringToQuat(OPQuatBBox.Text);
+                    quatResult = quatA.add(quatB);
+                    OPResultBox.Text = quatResult.ToString();
                     break;
                 case "subtract":
-                    result = quatA.subtract(quatB);
+                    quatA = parseStringToQuat(OPQuatABox.Text);
+                    quatB = parseStringToQuat(OPQuatBBox.Text);
+                    quatResult = quatA.subtract(quatB);
+                    OPResultBox.Text = quatResult.ToString();
                     break;
                 case "multiply":
-                    result = quatA.multiply(quatB);
+                    quatA = parseStringToQuat(OPQuatABox.Text);
+                    quatB = parseStringToQuat(OPQuatBBox.Text);
+                    quatResult = quatA.multiply(quatB);
+                    OPResultBox.Text = quatResult.ToString();
                     break;
                 case "divide":
-                    result = quatA.divide(quatB);
+                    quatA = parseStringToQuat(OPQuatABox.Text);
+                    quatB = parseStringToQuat(OPQuatBBox.Text);
+                    quatResult = quatA.divide(quatB);
+                    OPResultBox.Text = quatResult.ToString();
+                    break;
+                case "inverse":
+                    quatA = parseStringToQuat(IMQuatBox.Text);
+                    quatResult = quatA.inverse();
+                    IMResultBox.Text = quatResult.ToString();
+                    break;
+                case "magnitude":
+                    quatA = parseStringToQuat(IMQuatBox.Text);
+                    doubleResult = quatA.magnitude();
+                    IMResultBox.Text = doubleResult.ToString();
+                    break;
+                case "rotate":
+                    quatA = parseStringToQuat(RQuatABox.Text);
+                    quatB = parseStringToQuat(RQuatBBox.Text);
+                    double angleNumerator = 1.0f;
+
+                    if (!string.IsNullOrWhiteSpace(RAngleNumerator.Text))
+                        angleNumerator = Convert.ToDouble(RAngleNumerator.Text);
+
+                    double piInNumerator = 1.0f;
+                    
+                    if (RAngleIncludePIInNumerator.IsChecked.Value)
+                        piInNumerator = Math.PI;
+
+                    double angleDenominator = Convert.ToDouble(RAngleDenominator.Text);
+                    double angle = (angleNumerator * piInNumerator) / angleDenominator;
+
+                    quatResult = Quaternion.rotatePointByAngleAboutAxisVector(quatA, angle, quatB);
+                    RResultBox.Text = quatResult.ToString();
                     break;
                 default:
                     break;
             }
-
-            ResultBox.Text = result.ToString();
         }
 
         private void AddButtonClick(object sender, RoutedEventArgs e)
@@ -74,6 +111,21 @@ namespace qcalc_gui
         private void DivideButtonClick(object sender, RoutedEventArgs e)
         {
             Operation("divide");
+        }
+
+        private void InverseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Operation("inverse");
+        }
+
+        private void MagnitudeButtonClick(object sender, RoutedEventArgs e)
+        {
+            Operation("magnitude");
+        }
+
+        private void RotateButtonClick(object sender, RoutedEventArgs e)
+        {
+            Operation("rotate");
         }
 
         private Quaternion parseStringToQuat(String input)
